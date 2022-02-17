7eaba342e742a16d324fc307cb0ebe78f7d884e5
Oleksandr Bilanchuk <oleksandr.bilanchuk@ctdev.io>
2022-02-17 15:15:30 +0200

    Replaced int to BigInt.

--- bin/practice_2.dart
+++ bin/practice_2.dart
@@ -1,33 +1,22 @@
 import 'dart:core';
 
 abstract class FactorialCalculator {
-  Future<int> calculate(final int number);
+  Future<BigInt> calculate(final int number);
 }
 
 class FactorialCalculatorImpl implements FactorialCalculator {
   @override
-  Future<int> calculate(final int number) async {
-    if (number < 0) return Future.error("Can't calculate factorial for $number");
+  Future<BigInt> calculate(final int number) async {
+    if (number < 0) {
+      return Future.error("Can't calculate factorial for $number");
+    }
     return Future(() => _factorial(number));
   }
 
-  int _factorial(final int number) {
-    if (number == 0) return 1;
-    return number * _factorial(number - 1);
+  BigInt _factorial(final int number) {
+    if (number == 0) return BigInt.from(1);
+    BigInt bigNumber = BigInt.from(number);
+    return bigNumber * _factorial(number - 1);
   }
 
-/*  int _factorial(final int number) {
-    if (number == 0 || number == 1) return 1;
-
-    int previousValue = number - 1;
-    int result = number;
-
-    do {
-      result = result * previousValue;
-      previousValue = previousValue - 1;
-    } while (previousValue != 1);
-
-
-    return result;
-  }*/
 }
